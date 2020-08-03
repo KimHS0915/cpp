a4f41904ca35605c865700d571f6b75f3f1e5b7a
KimHS0915 <remember2552@gmail.com>
2020-08-03 22:42:16 +0900

    Chapter 05 Programming Exercises

---
+++ ch05/e06.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int count, sum, num;
+
+	printf("일한 날짜를 알려주세요.\n");
+	scanf("%d", &num);
+	count = 0;
+	sum = 0;
+	while (count++ < num)
+		sum = sum + count * count;
+	printf("합 = %d\n", sum);
+
+	return 0;
+}
